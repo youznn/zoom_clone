d774fcc0a0342258b769a0bfa7eac88a1d24f154
DESKTOP-JO3QE3M\82108 <leejin914418@gmail.com>
2023-08-14 23:09:03 +0900

    WebSocket 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,41 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const messageList = document.querySelector("ul");
 
 socket.addEventListener("open", () => {
   console.log("connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("Just got this", message.data, "from the server");
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("disconnected from Server");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser");
-}, 10000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = " ";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = " ";
+}
+
+function makeMessage(type, payload) {
+  //string으로 바꿔줍니다.
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -13,15 +13,25 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app); //http server
 const wss = new WebSocketServer({ server }); //websocket server
+const sockets = [];
 
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("connected to Browser");
-  socket.on("close", () => {
-    console.log("disconnected from Browser");
+  socket.on("close", () => console.log("disconnected from Browser"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg); //javascript object 형식으로 변경해주기
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        ); //message배열의 payload 띄우기
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload; //socket에 닉네임을 넣어줌
+        break;
+    }
   });
-  socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
-  });
-  socket.send("hello~");
 });
 server.listen(3000, handleListen);
