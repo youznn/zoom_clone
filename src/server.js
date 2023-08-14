3842606834808300d80e4d6a7af63e206389c712
DESKTOP-JO3QE3M\82108 <leejin914418@gmail.com>
2023-08-14 17:52:03 +0900

    feat: server setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.get("/", (req, res) => res.render("home"));
+
 console.log("hello");
 
 app.listen("3000");
